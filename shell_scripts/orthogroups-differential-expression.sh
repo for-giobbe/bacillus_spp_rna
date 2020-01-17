cad730fabaa74b77129cf795dfd8695dbaa81f18
Giobbe Forni <gf6030@gadi-login-01.gadi.nci.org.au>
2020-01-17 15:08:54 +1100

    ortho-de

--- shell_scripts/orthogroups-differential-expression.sh
+++ shell_scripts/orthogroups-differential-expression.sh
@@ -1,21 +1,30 @@
 
-	for i in ../alignemnts/5_sp_alignements/*fasta; 
+	for i in *aln;
 
-		do printf "$i "; 
+		do OG=$(echo $i | awk -F "." '{print $1}'); printf $OG;
 
 		for j in $(grep ">" $i);
 
-			do transcript=$(echo $j | awk -F "_i" '{print $1}' | tr -d ">"); 
+			do transcript=$(echo $j | awk -F "_i" '{print $1}' | tr -d ">");
 
-			species=$(echo $j | awk -F ".p1|.p2" '{print $2}' | tr -d "_1"); 
+			species=$(echo $j | awk -F ".p[0-9]" '{print $2}' | tr -d "_1");
 
-			t=$(grep $transcript DE_DESeq2_$species/*results | awk '{print $1"\t"$6"\t"$NF}'); 
+			t=$(grep $transcript $species".result" | awk '{print $1" "$6" "$NF}');
 
-			printf "$species $t "; 
+			printf " $species  $t" >> test.tmp;
 
-		done ; 
+		done ;
 
-	echo -e "\n"; 
+	printf " $OG \n" >> test.tmp; echo -e "";
 
-	done
+	done;
 
+
+
+	while read line; 
+
+		do echo $line | awk '/BAT TR*/ && /BRO TR*/ && /BGM TR*/'; 
+
+	done < test.tmp > orthogroups-de.tab;
+
+	rm *test.tmp
